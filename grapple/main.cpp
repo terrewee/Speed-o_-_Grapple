7d666b19fdfc7df2dec1393475d440b2b0b21a19
Terrewee <terrewee@live.com>
2019-04-02 12:05:35 +0200

    mains gemaakt

---
+++ grapple/main.cpp
@@ -0,0 +1,20 @@
+#include "BrickPi3.h" // for BrickPi3
+#include <stdio.h>      // for printf
+#include <unistd.h>     // for usleep
+#include <signal.h>     // for catching exit signals
+
+BrickPi3 BP;
+
+void exit_signal_handler(int signo);
+
+int main(){
+    return 0;
+}
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    BP.reset_all();    // Reset everything so there are no run-away motors
+    exit(-2);
+  }
+}
