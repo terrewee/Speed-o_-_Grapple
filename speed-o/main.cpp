00c1d4b55e1d41b9217f93078022f86736a503fb
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2019-04-02 12:45:28 +0200

    Grid creation

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -2,10 +2,78 @@
 #include <stdio.h>      // for printf
 #include <unistd.h>     // for usleep
 #include <signal.h>     // for catching exit signals
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
 
 BrickPi3 BP;
 
+struct coordinates{
+  int x;
+  int y;
+};
+
 void exit_signal_handler(int signo);
+
+// vector<vector<bool>> getGrid(){
+//     vector<vector<bool>> grid= {{}};
+//     int x = 0;
+//     int y = 0;
+    
+//     cout << "Please give the relative x coordinate of the object to be found." < <endl;
+//     cin >> x;
+//     cout << "Please give the relative y coordinate of the object to be found." < <endl;
+//     cin >> y;
+    
+//     grid = makeGrid(x, y);
+    
+//     return grid;
+// }
+
+vector<vector<bool>> makeGrid(coordinates userInput) {
+    //Makes a grid from x and y input.
+    vector<vector<bool>> grid = { {} };
+
+    for (int i = 0; i < y + 2; i++){
+        vector<bool> tempRow = {};
+
+        for (int j = 0; j < x + 2; j++){
+            tempRow.push_back(true);
+        }
+
+        grid.push_back(tempRow);
+    }
+
+    //Prints the grid to test it, this can be deleted later on.
+    // for (size_t k = 0; k < grid.size(); k++) {
+    //     for (size_t l = 0; l < grid[k].size(); l++) {
+    //         cout << grid[k][l];
+    //     }
+    //     cout << endl;
+    // }
+
+    return grid;
+}
+
+vector<vector<bool>> getGrid(){
+  vector<vector<bool>> grid= {{}};
+  coordinates userInput;
+    
+  cout << "Please give the relative x coordinate of the object to be found." < <endl;
+  cin >> userInput.x;
+  cout << "Please give the relative y coordinate of the object to be found." < <endl;
+  cin >> userInput.y;
+    
+  grid = makeGrid(coordinates);
+    
+  return grid;
+}
+
+
+
 
 int main(){
     return 0;
