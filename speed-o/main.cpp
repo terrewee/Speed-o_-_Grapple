77dc4fbd516cd8f95b37e972db8727355e232c6c
FreekigPersoon <joram.vanleeuwen@student.hu.nl>
2019-04-02 13:37:51 +0200

    v0.1 oppakken

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -1,20 +1,73 @@
-#include "BrickPi3.h" // for BrickPi3
-#include <stdio.h>      // for printf
-#include <unistd.h>     // for usleep
+#include "BrickPi3.h"   // for BrickPi3                                                     //TE DOEN
+#include <iostream>     // for printf
+#include <unistd.h>     // for usleep and sleep
 #include <signal.h>     // for catching exit signals
+#include <iomanip>		// for setw and setprecision
+
+using namespace std;
+
+sensor_color_t Color1;                                                                      //check de ports voor cohesie in sensornamen
+sensor_ultrasonic_t Ultrasonic2;
+sensor_touch_t Touch3;
+sensor_light_t Light4;
 
 BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
-int main(){
-    return 0;
+void setSensors()                                                                           //check of de ports juist ingesteld staan
+{
+	BP.set_sensor_type(PORT_1,SENSOR_TYPE_NXT_COLOR_FULL);
+	BP.set_sensor_type(PORT_2,SENSOR_TYPE_NXT_ULTRASONIC);
+	BP.set_sensor_type(PORT_3,SENSOR_TYPE_NXT_LIGHT_ON);
+	BP.set_sensor_type(PORT_4,SENSOR_TYPE_NXT_COLOR_FULL);
 }
 
-// Signal handler that will be called when Ctrl+C is pressed to stop the program
-void exit_signal_handler(int signo){
-  if(signo == SIGINT){
-    BP.reset_all();    // Reset everything so there are no run-away motors
-    exit(-2);
-  }
+void fwd(int lspd , int rspd)                                                               //check of de ports de juisten zijn
+{                                                                                           //check of de waardes nog negatief moeten zijn
+	lspd *=-1;
+	rspd *=-1;
+	BP.set_motor_power(PORT_B, lspd);
+	BP.set_motor_power(PORT_C, rspd);
+}
+
+void detecteerObject(int kleurVanObject)
+{
+    if(Ultrasonic2.cm <= 10 && Color1.color == kleurVanObject) //10cm als voorbeeld -- kan worden aangepast
+    {}                                                                                      //rijd totdat het object onder de klauw ligt -- encoder?
+}
+
+void grijpObject(int motorKracht)
+{
+    if(Touch3 == true)                                                                      //check hoe touch werkt
+    {
+        BP.set_motor_power(PORT_, motorKracht)                                              //set motorport voor sluiten grijper
+        sleep(2);
+        BP.set_motor_power(PORT_, motorKracht)                                              //set motorport voor omhoog halen grijper
+    }
+}
+
+void exit_signal_handler(int signo)
+{
+if(signo == SIGINT)
+    {
+        BP.reset_all();
+        exit(-2);
+    }
+}
+
+int main()
+{
+    signal(SIGINT, exit_signal_handler);
+  	BP.detect();
+  	BP.set_motor_limits(PORT_B, 60, 0);                                                     //check of de ports de juisten zijn
+	BP.set_motor_limits(PORT_C, 60, 0);
+	setSensors();
+
+    while(true)
+    {
+        detecteerObject();                                                                  //set kleurcode
+        sleep(3);
+        grijpObject();                                                                      //set motorkracht
+    }
 }
