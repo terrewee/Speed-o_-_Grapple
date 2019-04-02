d6b190d5033d7b7d564263966eeda6bbd4baee40
DuurAlblas <duur.alblas@gmail.com>
2019-04-02 14:38:42 +0200

    Created function to output sensor values

--- Sensor_check/main.cpp
+++ Sensor_check/main.cpp
@@ -10,7 +10,11 @@ BrickPi3 BP;
 bool battery = true;    //battery level function
 
 void exit_signal_handler(int signo);
-
+/*
+  Author:       Duur
+  Description:  setSensors set all the sensors for a specific robot
+                and immediatly sets them.
+*/
 void setSensors(){
   //BP.set_sensor_type();
 }
@@ -20,8 +24,6 @@ void setSensors(){
   Description:  Bateryscheck which changes the
                 global bool battery to false if battery is low
 */
-
-
 void batteryLevel(void){
   //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
   while(true){
@@ -38,10 +40,71 @@ void batteryLevel(void){
 
 /*
   Author:       Maaike & Duur
-  Description:
+  Description:  Asks the user to supply a port and a sensor type to check the output
+                of said function for a certain amount of time.
 */
-
 void checkSensor(){
+  sensor_color_t        Color;
+  sensor_ultrasonic_t   Ultrasonic;
+  sensor_light_t        Light;
+  sensor_touch_t        Touch;
+
+  int portNr = 0;
+  uint8_t portValue = 0;
+  int sensorNr = 0;
+  int nTimes = 1;
+
+  cout << "Geef poort nummer (1-4): " << endl;
+  cin >> portNr;
+  cout << "Geef sensor type: " << endl;
+  cout << "1: Color" << endl;
+  cout << "2: Ultrasonic" << endl;
+  cout << "3: Light" << endl;
+  cout << "4: Touch" << endl;
+  cin >> sensorNr;
+  cout << "Hoe vaak checken: " << endl;
+  cin >> nTimes;
+
+  if(portNr == 1) portValue = 0x01;
+  else if (portNr == 2) portValue = 0x02;
+  else if (portNr == 3) portValue = 0x03;
+  else if (portNr == 4) portValue = 0x04;
+  else {
+    cout << "Geen juiste keuze ontvangen.";
+    return;
+  }
+
+  switch(sensorNr){
+    case 1:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Color) == 0){
+          cout << " red : " << Color.reflected_red;
+          cout << " green : " << Color.reflected_green;
+          cout << " blue : " << Color.reflected_blue;
+          cout << " ambient : " << Color.ambient << endl;
+          cout << " color code : " << (int) Color.color << endl;
+        }
+      }
+    case 2:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Ultrasonic) == 0){
+          cout << Ultrasonic.cm << " cm" << endl;
+        }
+      }
+    case 3:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Light) == 0){
+          cout << " ambient : " << Light.ambient << endl;
+          cout << " reflected : " << Light.reflected << endl;
+        }
+      }
+    case 4:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Touch) == 0){
+          cout << "pressed : " << Touch.pressed << endl;
+        }
+      }
+  }
 
 }
 
