6af5c4bc223bab9910733efbedd4c172311aa0cd
mHovenkampV1R <43273774+mHovenkampV1R@users.noreply.github.com>
2019-04-02 12:15:31 +0200

    Eerste test batterycheck

--- Battery_check/main.cpp
+++ Battery_check/main.cpp
@@ -7,8 +7,17 @@ BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
+void batteryLevel(void){
+  printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  printf("9v voltage      : %.3f\n", BP.get_voltage_9v());
+  printf("5v voltage      : %.3f\n", BP.get_voltage_5v());
+  printf("3.3v voltage    : %.3f\n", BP.get_voltage_3v3());
+}
+
+
 int main(){
-    return 0;
+  batteryLevel();
+  return 0;
 }
 
 // Signal handler that will be called when Ctrl+C is pressed to stop the program
