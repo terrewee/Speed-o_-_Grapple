3f811dac727529568d7530291d2272f12e8c37ec
DuurAlblas <duur.alblas@gmail.com>
2019-04-02 14:13:48 +0200

    Created new branch and directory for the sensor check function

---
+++ Sensor_check/main.cpp
@@ -0,0 +1,63 @@
+#include "BrickPi3.h"   // for BrickPi3
+#include <iostream>     // voor printen
+#include <stdio.h>      // for printf
+#include <unistd.h>     // for usleep
+#include <signal.h>     // for catching exit signals
+#include <thread>       // thread
+
+using namespace std;
+BrickPi3 BP;
+bool battery = true;    //battery level function
+
+void exit_signal_handler(int signo);
+
+void setSensors(){
+  //BP.set_sensor_type();
+}
+
+/*
+  Author:       Maaike & Duur
+  Description:  Bateryscheck which changes the
+                global bool battery to false if battery is low
+*/
+
+
+void batteryLevel(void){
+  //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  while(true){
+    if(BP.get_voltage_battery() <= 9.0){
+      cout << "Yeeter de yoot de batterij is dood. T_T" << endl;
+      ::battery = false;
+    }
+    else{
+      ::battery = true;
+    }
+    sleep(5);
+  }
+}
+
+/*
+  Author:       Maaike & Duur
+  Description:
+*/
+
+void checkSensor(){
+
+}
+
+int main(){
+  //thread checkBattery (batteryLevel);
+  //setSensors();
+  while(true){
+    sleep(5);
+  }
+  return 0;
+}
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    BP.reset_all();    // Reset everything so there are no run-away motors
+    exit(-2);
+  }
+}
