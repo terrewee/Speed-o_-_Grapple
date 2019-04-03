35dbe19f4c9e4dd0198dd88e513e17a0ff43d9aa
mHovenkampV1R <43273774+mHovenkampV1R@users.noreply.github.com>
2019-04-03 13:14:52 +0200

    Aanmaken mapje Obstakel

---
+++ Obstakel/main.cpp
@@ -0,0 +1,155 @@
+#include "BrickPi3.h" // for BrickPi3
+#include <stdio.h>      // for printf
+#include <unistd.h>     // for usleep
+#include <signal.h>     // for catching exit signals
+
+BrickPi3 BP;
+
+void exit_signal_handler(int signo);
+
+/*
+  Author:       Duur
+  Description:  setSensors set all the sensors for a specific robot
+                and immediatly sets them.
+*/
+void setSensors(){
+  //BP.set_sensor_type();
+}
+
+/*
+  Author:       Maaike & Duur
+  Description:  Bateryscheck which changes the
+                global bool battery to false if battery is low
+*/
+void batteryLevel(void){
+  //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  while(true){
+    if(BP.get_voltage_battery() <= 9.0){
+      cout << "Yeeter de yoot de batterij is dood. T_T" << endl;
+      ::battery = false;
+    }
+    else{
+      ::battery = true;
+    }
+    sleep(5);
+  }
+}
+
+/*
+  Author:       Maaike & Duur
+  Description:  Asks the user to supply a port and a sensor type to check the output
+                of said function for a certain amount of time.
+*/
+void checkSensor(){
+  sensor_color_t        Color;
+  sensor_ultrasonic_t   Ultrasonic;
+  sensor_light_t        Light;
+  sensor_touch_t        Touch;
+
+  int portNr = 0;
+  uint8_t portValue = 0;
+  int sensorNr = 0;
+  int nTimes = 1;
+
+  cout << "Geef poort nummer (1-4): " << endl;
+  cin >> portNr;
+  cout << "Geef sensor type: " << endl;
+  cout << "1: Color" << endl;
+  cout << "2: Ultrasonic" << endl;
+  cout << "3: Light" << endl;
+  cout << "4: Touch" << endl;
+  cin >> sensorNr;
+  cout << "Hoe vaak checken: " << endl;
+  cin >> nTimes;
+
+  if(portNr == 1) portValue = 0x01;
+  else if (portNr == 2) portValue = 0x02;
+  else if (portNr == 3) portValue = 0x03;
+  else if (portNr == 4) portValue = 0x04;
+  else {
+    cout << "Geen juiste keuze ontvangen.";
+    return;
+  }
+
+  switch(sensorNr){
+    case 1:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Color) == 0){
+          cout << " red : " << Color.reflected_red;
+          cout << " green : " << Color.reflected_green;
+          cout << " blue : " << Color.reflected_blue;
+          cout << " ambient : " << Color.ambient << endl;
+          cout << " color code : " << (int) Color.color << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 2:
+      cout << "Case 2" << endl;
+      for (int i = 0; i < nTimes; ++i) {
+        cout << "In de case 2 loop nr : " << i << endl;
+        if(BP.get_sensor(portValue,Ultrasonic) == 0){
+          cout << Ultrasonic.cm << " cm" << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 3:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Light) == 0){
+          cout << " ambient : " << Light.ambient << endl;
+          cout << " reflected : " << Light.reflected << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 4:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Touch) == 0){
+          cout << "pressed : " << Touch.pressed << endl;
+        }
+        sleep(5);
+      }
+      break;
+  }
+
+}
+
+bool battery = true;    //battery level function
+/* 
+  Author:       Maaike & Duur
+  Description:  Bateryscheck which changes the 
+                global bool battery to false if battery is low
+*/
+
+
+void batteryLevel(void){
+  //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  while(true){
+    if(BP.get_voltage_battery() <= 9.0){
+      cout << "Yeeter de yoot de batterij is dood. T_T" << endl;
+      ::battery = false;
+    }
+    else{
+      ::battery = true;
+    }
+    sleep(5);
+  }
+}
+
+
+int main(){
+  thread checkBattery (batteryLevel);
+  while(true){
+    sleep(5);
+  }
+  return 0;
+}
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    BP.reset_all();    // Reset everything so there are no run-away motors
+    exit(-2);
+  }
+}
