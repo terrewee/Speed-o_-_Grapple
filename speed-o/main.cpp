4f0ebff578bb0e30356379c89834ab4e470ea3d2
FreekigPersoon <joram.vanleeuwen@student.hu.nl>
2019-04-03 15:57:13 +0200

    Finale test versie -- nog te testen

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -15,6 +15,11 @@ BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
+/*
+	Author		:	Joram van Leeuwen
+	Description	:
+		Simpele code om twee motoren eenmaal aan te sturen.
+*/
 void fwd(int lspd , int rspd)
 {
 	BP.set_motor_power(PORT_B, lspd);
@@ -22,31 +27,43 @@ void fwd(int lspd , int rspd)
 }
 
 /*
-	Author		:	Joram van Leeuwen, Duur Alblas
-	Description :
-		Short code to set a single motor encoder.
+	Author 		:	Duur Alblas
+	Description	:
+		Initialize all sensors
 */
+void setSensors(){
+	BP.set_sensor_type(PORT_1,SENSOR_TYPE_NXT_COLOR_FULL);
+	BP.set_sensor_type(PORT_2,SENSOR_TYPE_NXT_ULTRASONIC);
+	BP.set_sensor_type(PORT_3,SENSOR_TYPE_NXT_LIGHT_ON);
+	BP.set_sensor_type(PORT_4,SENSOR_TYPE_NXT_COLOR_FULL);
+}
 
 /*
 	Author		:	Joram van Leeuwen
-	Description :
+	Description	:
 		Code om een motor encoder te lezen -- alweer.
 */
-void readEncoder()
+void readEncoder()				// port is gezet op A
 {
 	BP.offset_motor_encoder(PORT_A, BP.get_motor_encoder(PORT_A));
 	int32_t encoderA = BP.get_motor_encoder(PORT_A);
 	cout << "Motor A: " << endl << encoderA << endl;
 }
 
+/*
+	Author:		:	Joram van Leeuwen, Duur Alblas
+	Description	:
+		Short code to set a single motor encoder.
+*/
 void encodeMotor(int32_t pos)
 {
 	BP.set_motor_position_relative(PORT_A, pos);
 }
 
 /*
-    Author: Joram van Leeuwen
-    Description: Grijpt een object in de klauw indien de touch sensor iets aanraakt. Dit is een puur iets waarvan ik niet zeker ben of het werkt
+	Author		:	Joram van Leeuwen, Stef Ottenhof
+	Description	:
+		Twee functies voor het omhoog en omlaag halen van de klauw.
 */
 void klauwNaarBeneden()
 {
@@ -73,7 +90,7 @@ int main()
 {
 	signal(SIGINT, exit_signal_handler);
   	BP.detect();
-	//setSensors();
+	setSensors();
 	klauwNaarBeneden();
 	sleep(10);				// sleep nu op 10 - pas aan zoals noodzakelijk is.
 	klauwOmhoog();
