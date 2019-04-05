1cfcd0427c8f66e7a255206884b88180382c4723
DuurAlblas <duur.alblas@gmail.com>
2019-04-05 14:04:12 +0200

    Went back to all code and placed set sensor somewhere else

--- Obstakel/main.cpp
+++ Obstakel/main.cpp
@@ -4,286 +4,265 @@
 #include <signal.h>     // for catching exit signals
 #include <iostream>     //voor cout enz.
 
+BrickPi3 BP;
 using namespace std;
 
-BrickPi3 BP;
-
 void exit_signal_handler(int signo);
-
 bool battery = true;    //battery level function
-/*
-  Author:       Duur
-  Description:  setSensors set all the sensors for a specific robot
-                and immediatly sets them.
-*/
-void setSensors(){
-  BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_ULTRASONIC);
-}
-
-void readme(){
-  sensor_ultrasonic_t ultraS;
-  while(true){
-    if(BP.get_sensor(PORT_2, ultraS) == 0){
-      cout << ultraS.cm << " CM" << endl;
-    } else {
-      cout << "Nope." << endl;
-    }
-    sleep(1);
-  }
-}
-
-
 /*
   Author:       Mathilde, Maaike
   Description:  Functie voor het detecteren van obstakels en de
                 ontwijking hiervan.
 */
 
-// struct range {
-//   bool obstakelInRangeLeft = false;
-//   bool obstakelInRangeRight = false;
-//   bool obstakelInRangeForward = false;
-//   bool obstakelcurrent= false;
-// };
+struct range {
+  bool obstakelInRangeLeft = false;
+  bool obstakelInRangeRight = false;
+  bool obstakelInRangeForward = false;
+  bool obstakelcurrent= false;
+};
 
 float check(){
+  BP.set_sensor_type(PORT_4, SENSOR_TYPE_NXT_ULTRASONIC);
   sensor_ultrasonic_t Ultrasonic;
   int som = 0;
-  int amount = 20;
-  for(unsigned int i = 0; i < amount; i++){
+  int countedValues = 0;
+  for(unsigned int i = 0; i < 6; i++){
     if(BP.get_sensor(PORT_3, Ultrasonic) == 0){
-      cout << "Afstand " << (int)Ultrasonic.cm << endl;
-      som += (int)Ultrasonic.cm;
-    }else{
+      cout << "Afstand " << (int) Ultrasonic.cm << endl;
+      som += (int) Ultrasonic.cm;
+      countedValues ++ ;
+    }
+    else{
       cout << "FUCK" << endl;
     }
-    usleep(50000);
+    sleep(1);
   }
-  return som/amount;
+  return som/countedValues;
 }
 
-// void stop(){
-//   //reset de motor dps
-//   BP.set_motor_dps(PORT_A, 0);
-// }
-// void lookLeft(range obstakel){
-//   cout << "left" << endl;
+void stop(){
+  //reset de motor dps
+  BP.set_motor_dps(PORT_A, 0);
+}
+void lookLeft(range obstakel){
+  cout << "left" << endl;
 
-//   //spin left
-//   BP.set_motor_dps(PORT_A, -60);
-//   sleep(2);
-//   stop();
+  //spin left
+  BP.set_motor_dps(PORT_A, -60);
+  sleep(2);
+  stop();
 
-//   float gemiddelde = check();
-//   if (gemiddelde <= 10.0){
-//     obstakel.obstakelInRangeLeft = true;
-//   }
-//   else{
-//     obstakel.obstakelInRangeLeft = false;
-//   }
+  float gemiddelde = check();
+  if (gemiddelde <= 10.0){
+    obstakel.obstakelInRangeLeft = true;
+  }
+  else{
+    obstakel.obstakelInRangeLeft = false;
+  }
 
-//   //reset to middle
-//   BP.set_motor_dps(PORT_A, 60);
-//   sleep(2);
-//   stop();
-// }
+  //reset to middle
+  BP.set_motor_dps(PORT_A, 60);
+  sleep(2);
+  stop();
+}
 
-// void lookRight(range obstakel){
-//   cout << "right" << endl;
+void lookRight(range obstakel){
+  cout << "right" << endl;
 
-//   //spin right.
-//   BP.set_motor_dps(PORT_A, 60);
-//   sleep(2);
-//   stop();
+  //spin right.
+  BP.set_motor_dps(PORT_A, 60);
+  sleep(2);
+  stop();
 
-//   float gemiddelde = check();
-//   if (gemiddelde <= 10.0){
-//     obstakel.obstakelInRangeLeft = true;
-//   }
-//   else{
-//     obstakel.obstakelInRangeLeft = false;
-//   }
+  float gemiddelde = check();
+  if (gemiddelde <= 10.0){
+    obstakel.obstakelInRangeLeft = true;
+  }
+  else{
+    obstakel.obstakelInRangeLeft = false;
+  }
 
-//   //reset to middle
-//   BP.set_motor_dps(PORT_A, -60);
-//   sleep(2);
-//   stop();
-// }
+  //reset to middle
+  BP.set_motor_dps(PORT_A, -60);
+  sleep(2);
+  stop();
+}
 
-// void lookForward(range obstakel){
-//   cout << "forward" << endl;
+void lookForward(range obstakel){
+  cout << "forward" << endl;
 
-//   float gemiddelde = check();
-//   if (gemiddelde <= 10.0){
-//     obstakel.obstakelInRangeForward = true;
-//   }
-//   else{
-//     obstakel.obstakelInRangeForward = false;
-//   }
-// }
+  float gemiddelde = check();
+  if (gemiddelde <= 10.0){
+    obstakel.obstakelInRangeForward = true;
+  }
+  else{
+    obstakel.obstakelInRangeForward = false;
+  }
+}
 
-// void obstakelDetectie(range obstacle){
-//   //main van obstakel
-//     stop();
+void obstakelDetectie(range obstacle){
+  //main van obstakel
+    stop();
 
-//     lookForward(obstacle);
+    lookForward(obstacle);
+    sleep(5);
+    lookLeft(obstacle);
+    sleep(5);
+    lookRight(obstacle);
+    // lookForward(Ultrasonic, obstacle);
+    // stop();
+    // while (obstacle.obstakelInRangeForward== true){
+    //     stop();
+    //     lookLeft(Ultrasonic, obstacle);
+    //     if (obstacle.obstakelInRangeLeft == true){
+    //         cout << "cant go left" << endl;
+    //     }
+    //     else{
+    //         cout << "can go left" << endl;
+    //     }
+    //     sleep (1);
+    //     lookRight(Ultrasonic, obstacle);
+    //     if (obstacle.obstakelInRangeRight == true){
+    //         cout << "cant go right" << endl;
+    //     }
+    //     else{
+    //         cout << "can go right" << endl;
+    //     }
+    //     sleep (1);
 
-//     lookLeft(obstacle);
-
-//     lookRight(obstacle);
-//     // lookForward(Ultrasonic, obstacle);
-//     // stop();
-//     // while (obstacle.obstakelInRangeForward== true){
-//     //     stop();
-//     //     lookLeft(Ultrasonic, obstacle);
-//     //     if (obstacle.obstakelInRangeLeft == true){
-//     //         cout << "cant go left" << endl;
-//     //     }
-//     //     else{
-//     //         cout << "can go left" << endl;
-//     //     }
-//     //     sleep (1);
-//     //     lookRight(Ultrasonic, obstacle);
-//     //     if (obstacle.obstakelInRangeRight == true){
-//     //         cout << "cant go right" << endl;
-//     //     }
-//     //     else{
-//     //         cout << "can go right" << endl;
-//     //     }
-//     //     sleep (1);
-
-//     // }
-// }
+    // }
+}
 
 
-// /*
-//   Author:       Maaike & Duur
-//   Description:  Bateryscheck which changes the
-//                 global bool battery to false if battery is low
-// */
-// void batteryLevel(void){
-//   //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
-//   while(true){
-//     if(BP.get_voltage_battery() <= 9.0){
-//       cout << "Yeeter de yoot de batterij is dood. T_T" << endl;
-//       ::battery = false;
-//     }
-//     else{
-//       ::battery = true;
-//     }
-//     sleep(5);
-//   }
-// }
+/*
+  Author:       Maaike & Duur
+  Description:  Bateryscheck which changes the
+                global bool battery to false if battery is low
+*/
+void batteryLevel(void){
+  //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  while(true){
+    if(BP.get_voltage_battery() <= 9.0){
+      cout << "Yeeter de yoot de batterij is dood. T_T" << endl;
+      ::battery = false;
+    }
+    else{
+      ::battery = true;
+    }
+    sleep(5);
+  }
+}
 
-// /*
-//   Author:       Maaike & Duur
-//   Description:  Asks the user to supply a port and a sensor type to check the output
-//                 of said function for a certain amount of time.
-// */
-// void checkSensor(){
-//   sensor_color_t        Color;
-//   sensor_ultrasonic_t   Ultrasonic;
-//   sensor_light_t        Light;
-//   sensor_touch_t        Touch;
+/*
+  Author:       Maaike & Duur
+  Description:  Asks the user to supply a port and a sensor type to check the output
+                of said function for a certain amount of time.
+*/
+void checkSensor(){
+  sensor_color_t        Color;
+  sensor_ultrasonic_t   Ultrasonic;
+  sensor_light_t        Light;
+  sensor_touch_t        Touch;
 
-//   int portNr = 0;
-//   uint8_t portValue = 0;
-//   int sensorNr = 0;
-//   int nTimes = 1;
+  int portNr = 0;
+  uint8_t portValue = 0;
+  int sensorNr = 0;
+  int nTimes = 1;
 
-//   cout << "Geef poort nummer (1-4): " << endl;
-//   cin >> portNr;
-//   cout << "Geef sensor type: " << endl;
-//   cout << "1: Color" << endl;
-//   cout << "2: Ultrasonic" << endl;
-//   cout << "3: Light" << endl;
-//   cout << "4: Touch" << endl;
-//   cin >> sensorNr;
-//   cout << "Hoe vaak checken: " << endl;
-//   cin >> nTimes;
+  cout << "Geef poort nummer (1-4): " << endl;
+  cin >> portNr;
+  cout << "Geef sensor type: " << endl;
+  cout << "1: Color" << endl;
+  cout << "2: Ultrasonic" << endl;
+  cout << "3: Light" << endl;
+  cout << "4: Touch" << endl;
+  cin >> sensorNr;
+  cout << "Hoe vaak checken: " << endl;
+  cin >> nTimes;
 
-//   if(portNr == 1) portValue = 0x01;
-//   else if (portNr == 2) portValue = 0x02;
-//   else if (portNr == 3) portValue = 0x03;
-//   else if (portNr == 4) portValue = 0x04;
-//   else {
-//     cout << "Geen juiste keuze ontvangen.";
-//     return;
-//   }
+  if(portNr == 1) portValue = 0x01;
+  else if (portNr == 2) portValue = 0x02;
+  else if (portNr == 3) portValue = 0x03;
+  else if (portNr == 4) portValue = 0x04;
+  else {
+    cout << "Geen juiste keuze ontvangen.";
+    return;
+  }
 
-//   switch(sensorNr){
-//     case 1:
-//       for (int i = 0; i < nTimes; ++i) {
-//         if(BP.get_sensor(portValue,Color) == 0){
-//           cout << " red : " << Color.reflected_red;
-//           cout << " green : " << Color.reflected_green;
-//           cout << " blue : " << Color.reflected_blue;
-//           cout << " ambient : " << Color.ambient << endl;
-//           cout << " color code : " << (int) Color.color << endl;
-//         }
-//         sleep(5);
-//       }
-//       break;
-//     case 2:
-//       cout << "Case 2" << endl;
-//       for (int i = 0; i < nTimes; ++i) {
-//         cout << "In de case 2 loop nr : " << i << endl;
-//         if(BP.get_sensor(portValue,Ultrasonic) == 0){
-//           cout << Ultrasonic.cm << " cm" << endl;
-//         }
-//         sleep(5);
-//       }
-//       break;
-//     case 3:
-//       for (int i = 0; i < nTimes; ++i) {
-//         if(BP.get_sensor(portValue,Light) == 0){
-//           cout << " ambient : " << Light.ambient << endl;
-//           cout << " reflected : " << Light.reflected << endl;
-//         }
-//         sleep(5);
-//       }
-//       break;
-//     case 4:
-//       for (int i = 0; i < nTimes; ++i) {
-//         if(BP.get_sensor(portValue,Touch) == 0){
-//           cout << "pressed : " << Touch.pressed << endl;
-//         }
-//         sleep(5);
-//       }
-//       break;
-//   }
+  switch(sensorNr){
+    case 1:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Color) == 0){
+          cout << " red : " << Color.reflected_red;
+          cout << " green : " << Color.reflected_green;
+          cout << " blue : " << Color.reflected_blue;
+          cout << " ambient : " << Color.ambient << endl;
+          cout << " color code : " << (int) Color.color << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 2:
+      cout << "Case 2" << endl;
+      for (int i = 0; i < nTimes; ++i) {
+        cout << "In de case 2 loop nr : " << i << endl;
+        if(BP.get_sensor(portValue,Ultrasonic) == 0){
+          cout << Ultrasonic.cm << " cm" << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 3:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Light) == 0){
+          cout << " ambient : " << Light.ambient << endl;
+          cout << " reflected : " << Light.reflected << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 4:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Touch) == 0){
+          cout << "pressed : " << Touch.pressed << endl;
+        }
+        sleep(5);
+      }
+      break;
+  }
 
-// }
+}
 
 int main(){
 //thread checkBattery (batteryLevel);
   BP.detect();
   setSensors();
- // range obstacle;
-  readme();
- // cout << check();
+  range obstacle;
 
-  // bool loop = true;
-  // while(loop){
-  //   int keuze;
-  //   cout << "Kies een funtie: "<< endl;
-  //   cout << "1. Getsensor *werkt niet todat setSensors() werkt" << endl;
-  //     cout << "2. Obstakel detectie" << endl;
-  //   cin >> keuze;
 
-  //   switch(keuze){
-  //     case 1:
-  //       //checkSensor();
-  //       break;
-  //     case 2:
-  //       //obstakelDetectie(obstacle);
-  //       break;
-  //     default:
-  //       cout << "Het programma word afgebroken" << endl;
-  //       loop = false;
-  //       break;
-  //   }
-  // }
+
+  bool loop = true;
+  while(loop){
+    int keuze;
+    cout << "Kies een funtie: "<< endl;
+    cout << "1. Getsensor *werkt niet todat setSensors() werkt" << endl;
+    cout << "2. Obstakel detectie" << endl;
+    cin >> keuze;
+
+    switch(keuze){
+      case 1:
+        checkSensor();
+        break;
+      case 2:
+        obstakelDetectie(obstacle);
+        break;
+      default:
+        cout << "Het programma word afgebroken" << endl;
+        loop = false;
+        break;
+    }
+  }
   return 0;
 }
 
