9cffd166b28c0939c1d541017aa90270fd3ef7f8
Mathilde <muj.tegel@gmail.com>
2019-04-09 15:12:38 +0200

    bp functies even uitgezet

--- grapple/Kleur_sensor.cpp
+++ grapple/Kleur_sensor.cpp
@@ -2,18 +2,18 @@
 #include <stdio.h>      // for printf
 #include <unistd.h>     // for usleep
 #include <signal.h>     // for catching exit signals
-#include <linux/spi/spidev.h>
+
 
 using namespace std;
-BrickPi3 BP;
+// BrickPi3 BP;
 
-BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
-sensor_color_t      Color1;
-BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_COLOR_FULL);
-sensor_color_t      Color2;
+// BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
+// sensor_color_t      Color1;
+// BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_COLOR_FULL);
+// sensor_color_t      Color2;
 
 
-int WhatIsInAColor (sensor_color_t){
+int WhatIsInAColor (){
 int color;
 cout << "Welke kleur heeft het object" << endl;
 cout << "1: Rood " << endl;
@@ -59,62 +59,34 @@ void color_object(nepwaarde,  WhatIsInAColor, int powerA, int power){
 }
 
 int main(){
-      signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
-  BP.detect();
-  BP.reset_all();
-  for (int i = 0; i < 5; ++i){
-    cout << ".";
-    if (i == 3){
-      setSensors();
-    }
-    sleep(1);
-  }
-  cout << endl << "Initialized" << endl;
+//       signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
+//   BP.detect();
+//   BP.reset_all();
+//   for (int i = 0; i < 5; ++i){
+//     cout << ".";
+//     if (i == 3){
+//       setSensors();
+//     }
+//     sleep(1);
+//   }
+//   cout << endl << "Initialized" << endl;
 
-  thread checkBattery (batteryLevel);
+//   thread checkBattery (batteryLevel);
 
-  while(true){
-    sleep(5);
-  }
+//   while(true){
+//     sleep(5);
+//   }
 
-  BP.reset_all();
-  return 0;
-}
-// Signal handler that will be called when Ctrl+C is pressed to stop the program
-void exit_signal_handler(int signo){
-  if(signo == SIGINT){
-    BP.reset_all();    // Reset everything so there are no run-away motors
-    exit(-2);
-  }
-}
+//   BP.reset_all();
+//   return 0;
+// }
+// // Signal handler that will be called when Ctrl+C is pressed to stop the program
+// void exit_signal_handler(int signo){
+//   if(signo == SIGINT){
+//     BP.reset_all();    // Reset everything so there are no run-away motors
+//     exit(-2);
+//   }
+// }
 
 WhatIsInAColor(sensor_color_t)
 }
-
-
-
-// void measure(sensor_color_t Color1, sensor_color_t Color4, int powerA, int powerB, sensor_ultrasonic_t ultrasonic, int &ticker){
-   
-//     if((BP.get_sensor(PORT_1, Color1) == 0)&&(BP.get_sensor(PORT_4, Color4) == 0)){
-//     cout << "Color1 " << (int) Color1.color << " Color4 " << (int) Color4.color << endl;
-  //      if     (Color1.color == 1 && Color4.color == 6)                         {rightcorrectie(powerA, powerB);}        //rechts wit links zwart
-//         else if(Color1.color == 2 && Color4.color == 6)                         {mediumRight(powerA, powerB);}
-//         else if((Color1.color > 2 && Color1.color < 5) && Color4.color == 1)    {slightRight(powerA, powerB);}
-        
-//         else if(Color1.color == 6 && Color4.color == 1)                         {leftcorrectie(powerA, powerB);}         //rechts zwart links wit
-//         else if(Color1.color == 6 && Color4.color == 2)                         {mediumLeft(powerA, powerB);}
-//         else if(Color1.color == 6 && (Color4.color > 2 && Color4.color < 5))    {slightLeft(powerA, powerB);}
-        
-//         else if(Color1.color == 6 && Color4.color == 6)                         {fwd(powerA, powerB);}                   //rechts zwart 
-//         else if(Color1.color == 1 && Color4.color == 1)                         {crossroad(powerA, powerB, Color1, Color4);}  
-        
-//         ticker++;
-//         cout << "ticker: " << ticker << endl;
-//     }
-// }
-
-// getline(cin, color);
-// if (color == 1)     { colorchoice = Color1.reflected_red;}
-// if (color == 2)     { colorchoice = Color1.reflected_blue;}
-// if (color == 3)     { colorchoice = Color1.reflected_green;} 
-// if (color == 4)     { colorchoice = Color1.ambient;}
