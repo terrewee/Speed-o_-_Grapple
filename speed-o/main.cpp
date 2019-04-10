8b7518664569cdcc0b2cefe3b8975ecb94e15189
StefanStegeman <s.stegeman1806@gmail.com>
2019-04-10 14:27:23 +0200

    merge voorkomen

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -16,18 +16,18 @@ BrickPi3 BP;
 void exit_signal_handler(int signo);
 
 int crossroad;
-/*
-bool crossroaddetectie(const sensor_color_t & Color2, const sensor_color_t & Color4){
+
+bool crossroaddetectie(sensor_color_t Color2, sensor_color_t Color4){
   ::crossroad = 0;
   while (true){
     if((BP.get_sensor(PORT_2, Color2) == 0)&&(BP.get_sensor(PORT_4, Color4) == 0)){
-      if      (Color2.color == 1 && Color4.color == 1){ usleep(0.5); ::crossroad++;}    //sleep zodat hij niet hetzelfde kruispunt 2 keer registreert
-      else if (Color2.color == 1 && Color4.color == 0){ usleep(0.5); ::crossroad++;}
+      if (Color2.color == 1 && Color4.color == 1){ usleep(0.5); ::crossroad++;}//sleep zodat hij niet hetzelfde kruispunt 2 keer registreert
+      else if (Color2.color == 1 && Color4.color == 0){usleep(0.5); ::crossroad++;}
       else if (Color2.color == 0 && Color4.color == 1){ usleep(0.5); ::crossroad++;}
     }
   }
 }
-*/
+
 
 void setSensors(){
 	BP.set_sensor_type(PORT_1,SENSOR_TYPE_NXT_COLOR_FULL);
