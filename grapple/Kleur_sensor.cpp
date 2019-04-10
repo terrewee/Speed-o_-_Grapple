ba0a510762f709c28423fd5ddfd47d687f9cb913
Mathilde <muj.tegel@gmail.com>
2019-04-10 11:46:02 +0200

    Ik heb de BP waardes en functies weer toegevoegd

--- grapple/Kleur_sensor.cpp
+++ grapple/Kleur_sensor.cpp
@@ -2,91 +2,101 @@
 #include <stdio.h>      // for printf
 #include <unistd.h>     // for usleep
 #include <signal.h>     // for catching exit signals
+#include <iostream>
+#include <string>
 
 
 using namespace std;
-// BrickPi3 BP;
 
-// BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
-// sensor_color_t      Color1;
-// BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_COLOR_FULL);
-// sensor_color_t      Color2;
+BrickPi3 BP;
+
+BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
+sensor_color_t      Color1;
+BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_COLOR_FULL);
+sensor_color_t      Color2;
 
 
-int WhatIsInAColor (){
-int color;
+int WhatIsInAColor (sensor_color_t,){
+int_fast64_t color;
+int colorchoice = 0;
 cout << "Welke kleur heeft het object" << endl;
-cout << "1: Rood " << endl;
-cout << "2: Blauw " << endl;
-cout << "3: Groen " << endl;
-cout << "4: Zwart " << endl;
-getline(cin, color);
-if (color == 1)     { colorchoice = 1;}
-if (color == 2)     { colorchoice = 2;}
-if (color == 3)     { colorchoice = 3;} 
-if (color == 4)     { colorchoice = 4;}
+cout << "1 : Rood " << endl;
+cout << "2 : Blauw " << endl;
+cout << "3 : Groen " << endl;
+cout << "4 : Zwart " << endl;
+cin >> color;
+cout << color;
+if (color == 1)     { colorchoice = Color1.reflected_red;}
+if (color == 2)     { colorchoice = Color1.reflected_blue;}
+if (color == 3)     { colorchoice = Color1.reflected_green;} 
+if (color == 4)     { colorchoice = Color1.ambient;}
 return colorchoice;
 }
 
-int nepwaarde (){
-    int gemeten;
-    cout << "Welke kleur heeft het object sensor" << endl;
-    cout << "1: Rood " << endl;
-    cout << "2: Blauw " << endl;
-    cout << "3: Groen " << endl;
-    cout << "4: Zwart " << endl;
-    getline(cin, gemeten);
-    if (gemeten == 1)     { gemetenwaarde = 1;}
-    if (gemeten == 2)     { gemetenwaarde  = 2;}
-    if (gemeten == 3)     { gemetenwaarde  = 3;} 
-    if (gemeten == 4)     { gemetenwaarde = 4;}
-    return gemetenwaarde;
+char FarbeNichtRichtig() {
+    string answer;
+    cout << "Dit object heeft niet de door u opgegeven kleur. Wilt u het object alsnog oppakken?" << endl;
+    cout << "Ja of Nee" << endl;
+    cin >> answer;
+    if (answer == "Ja" )        {cout << "hij rijd naar achteren; op pak functie;"<< endl;}
+    if (answer == "nee")        { cout << "Scout gaat weer zoeken"<< endl;} 
     
 }
 
-char FarbeNichtRichtig() {
-    string answer
-    cout << "Dit object heeft niet de door u opgegeven kleur. Wilt u het object alsnog oppakken?" << endl;
-    cout << "Ja of Nee"
-    getline( cin, answer)
-    if (answer == "Ja" )        {cout << "hij rijd naar achteren; op pak functie;"<< endl;}
-    if (answer == "nee")        { cout << "Penis"<< endl;} 
-
+// int nepwaarde (){
+//     int gemeten;
+//     int gemetenwaarde = 0;
+//     cout << "Welke kleur heeft het object sensor" << endl;
+//     cout << "1: Rood " << endl;
+//     cout << "2: Blauw " << endl;
+//     cout << "3: Groen " << endl;
+//     cout << "4: Zwart " << endl;
+//     cin >> gemeten;
+//     cout << gemeten;
+//     if (gemeten == 1)     { gemetenwaarde = 1;}
+//     if (gemeten == 2)     { gemetenwaarde  = 2;}
+//     if (gemeten == 3)     { gemetenwaarde  = 3;} 
+//     if (gemeten == 4)     { gemetenwaarde = 4;}
+//     return gemetenwaarde;
+     
 }
-void color_object(nepwaarde,  WhatIsInAColor, int powerA, int power){
-    if (gemetenwaarde == colorchoice)       { cout << "hij rijd naar achteren; op pak functie;" << endl;}
-    if (gemetenwaarde != colorchoice)       { FarbeNichtRichtig()}
+
+
+char color_object (BP.get_sensor(PORT_1, Color1, int colorchoice){
+    if (BP.get_sensor(PORT_1, Color1) == colorchoice)       { cout << "hij rijd naar achteren; op pak functie;" << endl;}
+    if (BP.get_sensor(PORT_1, Color1) != colorchoice)       { FarbeNichtRichtig();}
 }
 
 int main(){
-//       signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
-//   BP.detect();
-//   BP.reset_all();
-//   for (int i = 0; i < 5; ++i){
-//     cout << ".";
-//     if (i == 3){
-//       setSensors();
-//     }
-//     sleep(1);
-//   }
-//   cout << endl << "Initialized" << endl;
+      signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
+  BP.detect();
+  BP.reset_all();
+  for (int i = 0; i < 5; ++i){
+    cout << ".";
+    if (i == 3){
+      setSensors();
+    }
+    sleep(1);
+  }
+  cout << endl << "Initialized" << endl;
 
-//   thread checkBattery (batteryLevel);
+  thread checkBattery (batteryLevel);
 
-//   while(true){
-//     sleep(5);
-//   }
+  while(true){
+    sleep(5);
+  }
 
-//   BP.reset_all();
-//   return 0;
-// }
-// // Signal handler that will be called when Ctrl+C is pressed to stop the program
-// void exit_signal_handler(int signo){
-//   if(signo == SIGINT){
-//     BP.reset_all();    // Reset everything so there are no run-away motors
-//     exit(-2);
-//   }
-// }
+  BP.reset_all();
+  return 0;
+}
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    BP.reset_all();    // Reset everything so there are no run-away motors
+    exit(-2);
+  }
+}
 
-WhatIsInAColor(sensor_color_t)
+
+color_object (nepwaarde(), WhatIsInAColor());
 }
