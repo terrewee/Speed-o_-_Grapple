c997bf1e12391a2ced653a0a7308a6e17b31944c
Terrewee <terrewee@live.com>
2019-04-10 16:43:45 +0200

    removed shitty hidden files

--- grapple/main.cpp
+++ grapple/main.cpp
@@ -176,6 +176,47 @@ void iServer(){
   close(socketFD);
 }
 
+//---------------------------------------ARM---------------------------------------------
+
+void setSensors(){
+	BP.set_sensor_type(PORT_1,SENSOR_TYPE_NXT_COLOR_FULL);
+	BP.set_sensor_type(PORT_2,SENSOR_TYPE_NXT_ULTRASONIC);
+	BP.set_sensor_type(PORT_3,SENSOR_TYPE_NXT_LIGHT_ON);
+	BP.set_sensor_type(PORT_4,SENSOR_TYPE_NXT_COLOR_FULL);
+}
+
+void encodeMotor(int32_t pos){
+	BP.set_motor_position_relative(PORT_A, pos);
+}
+
+void brengNaarKantelPunt(){
+	BP.set_motor_limits(PORT_A, 40, 0);
+	encodeMotor(-50);
+}
+
+void gelijdelijkDownLoop(){
+	int32_t encoder = -50;
+	while(encoder > -110){
+		encodeMotor(-5);
+		usleep(500000);
+		encoder = encoder - 5;
+	}
+}
+
+void klauwOmhoog(){
+	BP.set_motor_limits(PORT_A, 50, 0);
+	encodeMotor(130);	// zelfde als totale neerwaartse beweging
+}
+
+void klauwOpen(){
+	BP.set_motor_limits(PORT_D, 60, 0);
+	BP.set_motor_position_relative(PORT_D, -180);
+}
+
+void klauwDicht(){
+	BP.set_motor_limits(PORT_D, 60, 0);
+	BP.set_motor_position_relative(PORT_D, 180);
+}
 
 //---------------------------------------MAIN---------------------------------------------
 
@@ -216,6 +257,30 @@ int main(){
         break;
     }
   }
+
+  if(signo == SIGINT)
+    {
+        BP.reset_all();
+        exit(-2);
+    }
+
+
+
+	signal(SIGINT, exit_signal_handler);
+  BP.detect();
+	setSensors();
+	klauwOpen();
+	sleep(1);
+	brengNaarKantelPunt();
+	sleep(1);
+	gelijdelijkDownLoop();
+	sleep(1);
+	klauwDicht();
+	sleep(1);
+	klauwOmhoog();
+	sleep(3);
+	BP.set_motor_power(PORT_A, 0);
+
 
   BP.reset_all();
   return 0;
