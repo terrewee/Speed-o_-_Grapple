d6286db4249f7fb7725a9f792ef71654990e853f
Terrewee <terrewee@live.com>
2019-04-11 14:29:20 +0200

    Update main.cpp

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -43,11 +43,82 @@ void exit_signal_handler(int signo){
   }
 }
 
-void checkSensor(){
+void setSensor(){
   BP.set_sensor_type(PORT_1,SENSOR_TYPE_NXT_ULTRASONIC);
   BP.set_sensor_type(PORT_2,SENSOR_TYPE_NXT_COLOR_FULL);
   BP.set_sensor_type(PORT_3,SENSOR_TYPE_NXT_LIGHT_ON);
   BP.set_sensor_type(PORT_4,SENSOR_TYPE_NXT_COLOR_FULL);
+}
+
+void checkSensor() {
+  sensor_color_t        Color;
+  sensor_ultrasonic_t   Ultrasonic;
+  sensor_light_t        Light;
+  sensor_touch_t        Touch;
+
+  int portNr = 0;
+  uint8_t portValue = 0;
+  int sensorNr = 0;
+  int nTimes = 1;
+
+  cout << "Geef poort nummer (1-4): " << endl;
+  cin >> portNr;
+  cout << "Geef sensor type: " << endl;
+  cout << "1: Color" << endl;
+  cout << "2: Ultrasonic" << endl;
+  cout << "3: Light" << endl;
+  cout << "4: Touch" << endl;
+  cin >> sensorNr;
+  cout << "Hoe vaak checken: " << endl;
+  cin >> nTimes;
+
+  if(portNr == 1) portValue = 0x01;
+  else if (portNr == 2) portValue = 0x02;
+  else if (portNr == 3) portValue = 0x03;
+  else if (portNr == 4) portValue = 0x04;
+  else { cout << "Geen juiste keuze ontvangen.";}
+
+  switch(sensorNr) {
+    case 1:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Color) == 0) {
+          cout << " red : " << Color.reflected_red;
+          cout << " green : " << Color.reflected_green;
+          cout << " blue : " << Color.reflected_blue;
+          cout << " ambient : " << Color.ambient << endl;
+          cout << " color code : " << (int) Color.color << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 2:
+      cout << "Case 2" << endl;
+      for (int i = 0; i < nTimes; ++i) {
+        cout << "In de case 2 loop nr : " << i << endl;
+        if(BP.get_sensor(portValue,Ultrasonic) == 0) {
+          cout << Ultrasonic.cm << " cm" << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 3:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Light) == 0) {
+          cout << " ambient : " << Light.ambient << endl;
+          cout << " reflected : " << Light.reflected << endl;
+        }
+        sleep(5);
+      }
+      break;
+    case 4:
+      for (int i = 0; i < nTimes; ++i) {
+        if(BP.get_sensor(portValue,Touch) == 0) {
+          cout << "pressed : " << Touch.pressed << endl;
+        }
+        sleep(5);
+      }
+      break;
+  }
 }
 
 //------------------------------------------CONNECTION-----------------------------------------------
@@ -778,7 +849,7 @@ int main(){
   for (int i = 0; i < 5; ++i){
     cout << ".";
     if (i == 3){
-      checkSensor();
+      setSensor();
     }
     sleep(1);
   }
@@ -794,12 +865,7 @@ int main(){
 	int32_t EncoderA = BP.get_motor_encoder(PORT_A);
 	int32_t EncoderB = BP.get_motor_encoder(PORT_B);
 	int32_t EncoderC = BP.get_motor_encoder(PORT_C);
-	int32_t EncoderD = BP.get_motor_encoder(PORT_D);
-
-	//Use the encoder value from motor A to control motors B, C, and D
-	BP.set_motor_power(PORT_B, EncoderA < 100 ? EncoderA > -100 ? EncoderA : -100 : 100);
-	BP.set_motor_dps(PORT_C, EncoderA);
-	BP.set_motor_position(PORT_D, EncoderA);	
+	int32_t EncoderD = BP.get_motor_encoder(PORT_D);	
 	
 	int uChoice;
 	char message[256];
@@ -840,8 +906,7 @@ int main(){
         checkSensor();
         break;
 			case 4:
-				
-				testFunctie(GP, grid);
+				// testFunctie(GP, grid);
 				resetCurrentLocation(GP);
 				followRoute(followedRoute, destinationArrived, GP, grid, obstakel);
 				cout << "followed route" << endl;
