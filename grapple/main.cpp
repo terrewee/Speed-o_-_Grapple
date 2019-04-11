748dea9b3879a91f7c1c95e2135332063b5922eb
DuurAlblas <duur.alblas@gmail.com>
2019-04-11 11:09:21 +0200

    Cleaned code ready to start putting it all together

--- grapple/main.cpp
+++ grapple/main.cpp
@@ -19,22 +19,19 @@ using namespace std;
 
 //---------------------------------------ESSENTIALS---------------------------------------------
 
-BrickPi3 BP;                  //initializing BP
-bool battery = true;          //battery level function
-bool running = true;          //Bool for turning off the program including threads
+BrickPi3 BP;                                                      // Initializing BP
+bool battery = true;                                              // Bool for the battery level function
+bool running = true;                                              // Bool for turning off the program including threads
 
+void exit_signal_handler(int signo);                              // For initializing the Ctrl + C exit handler
 
-void exit_signal_handler(int signo); // for initializing the Ctrl + C exit handler
-
-
-void setSensors(){
+void setSensors() {
     BP.set_sensor_type(PORT_1,SENSOR_TYPE_NXT_COLOR_FULL);
     BP.set_sensor_type(PORT_2,SENSOR_TYPE_NXT_COLOR_FULL);
     BP.set_sensor_type(PORT_3,SENSOR_TYPE_NXT_LIGHT_ON);
 }
 
-
-void resetMotor(){
+void resetMotor() {
     BP.set_motor_dps(PORT_A, 0);
     BP.set_motor_dps(PORT_B, 0);
     BP.set_motor_dps(PORT_C, 0);
@@ -50,8 +47,7 @@ void resetMotor(){
     sleep(1);
 }
 
-
-void checkSensor(){
+void checkSensor() {
   sensor_color_t        Color;
   sensor_ultrasonic_t   Ultrasonic;
   sensor_light_t        Light;
@@ -77,14 +73,12 @@ void checkSensor(){
   else if (portNr == 2) portValue = 0x02;
   else if (portNr == 3) portValue = 0x03;
   else if (portNr == 4) portValue = 0x04;
-  else {
-    cout << "Geen juiste keuze ontvangen.";
-  }
+  else { cout << "Geen juiste keuze ontvangen.";}
 
-  switch(sensorNr){
+  switch(sensorNr) {
     case 1:
       for (int i = 0; i < nTimes; ++i) {
-        if(BP.get_sensor(portValue,Color) == 0){
+        if(BP.get_sensor(portValue,Color) == 0) {
           cout << " red : " << Color.reflected_red;
           cout << " green : " << Color.reflected_green;
           cout << " blue : " << Color.reflected_blue;
@@ -98,7 +92,7 @@ void checkSensor(){
       cout << "Case 2" << endl;
       for (int i = 0; i < nTimes; ++i) {
         cout << "In de case 2 loop nr : " << i << endl;
-        if(BP.get_sensor(portValue,Ultrasonic) == 0){
+        if(BP.get_sensor(portValue,Ultrasonic) == 0) {
           cout << Ultrasonic.cm << " cm" << endl;
         }
         sleep(5);
@@ -106,7 +100,7 @@ void checkSensor(){
       break;
     case 3:
       for (int i = 0; i < nTimes; ++i) {
-        if(BP.get_sensor(portValue,Light) == 0){
+        if(BP.get_sensor(portValue,Light) == 0) {
           cout << " ambient : " << Light.ambient << endl;
           cout << " reflected : " << Light.reflected << endl;
         }
@@ -115,7 +109,7 @@ void checkSensor(){
       break;
     case 4:
       for (int i = 0; i < nTimes; ++i) {
-        if(BP.get_sensor(portValue,Touch) == 0){
+        if(BP.get_sensor(portValue,Touch) == 0) {
           cout << "pressed : " << Touch.pressed << endl;
         }
         sleep(5);
@@ -124,82 +118,70 @@ void checkSensor(){
   }
 }
 
-
-void batteryLevel(void){
+void batteryLevel() {
   //printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
-  while(true){
-    if(BP.get_voltage_battery() <= 9.0){
+  while(::running) {
+    if(BP.get_voltage_battery() <= 9.0) {
       cout << "De batterij is leeg. T_T" << endl;
       ::battery = false;
-    }
-    else{
+    } else {
       ::battery = true;
     }
     sleep(5);
   }
 }
 
-
 //---------------------------------------ARM---------------------------------------------
 
-void encodeMotorA(int32_t pos){
+void encodeMotorA(int32_t pos) {
 	BP.set_motor_position_relative(PORT_A, pos);
 }
 
-
-void brengNaarKantelPunt(){
+void brengNaarKantelPunt() {
 	BP.set_motor_limits(PORT_A, 40, 0);
 	encodeMotorA(-50);
 }
 
-
-void gelijdelijkDownLoop(){
+void gelijdelijkDownLoop() {
 	int32_t encoder = -50;
-	while(encoder > -110){
+	while(encoder > -110) {
 		encodeMotorA(-5);
 		usleep(500000);
 		encoder = encoder - 5;
 	}
 }
 
-
-void klauwOmhoog(){
+void klauwOmhoog() {
 	BP.set_motor_limits(PORT_A, 50, 0);
 	encodeMotorA(130);	// zelfde als totale neerwaartse beweging
 }
 
-
-void klauwOpen(){
+void klauwOpen() {
 	BP.set_motor_limits(PORT_D, 60, 0);
 	BP.set_motor_position_relative(PORT_D, -180);
 }
 
-
-void klauwDicht(){
+void klauwDicht() {
 	BP.set_motor_limits(PORT_D, 60, 0);
 	BP.set_motor_position_relative(PORT_D, 180);
 }
-
 
 //---------------------------------------COMMUNICATION---------------------------------------------
 
 int ComPortNr = 6969;         //Standaard Port number for communication
 char ComHostName[] = "dex2";  //Standaard Hostname for communication
 
-
-void SetComm(){
+void SetComm() {
     cout << endl << "Geef het poort-nummer op: ";
     cin >> ::ComPortNr; cout << endl;
 }
-
 
 void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-
-vector<char> iServer(){
+vector<char> iServer() {
 
     int socketFD, newSocketFD, n;
     socklen_t clilen;
@@ -211,7 +193,7 @@ vector<char> iServer(){
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(::ComPortNr);
 
-    if (bind(socketFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+    if (bind(socketFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         error("ERROR on binding");
     }
 
@@ -221,14 +203,14 @@ vector<char> iServer(){
     clilen = sizeof(cli_addr);
     newSocketFD = accept(socketFD, (struct sockaddr *) &cli_addr, &clilen);
 
-    if (newSocketFD < 0){
+    if (newSocketFD < 0) {
         error("ERROR on accept");
     }
 
     bzero(buffer,256);
     n = read(newSocketFD,buffer,255);
 
-    if (n < 0){
+    if (n < 0) {
         error("ERROR reading from socket");
     }
     // Received message in the buffer.
@@ -250,7 +232,7 @@ vector<char> iServer(){
     ssTemp << buffer;
     ssTemp >> target;
 
-    for(unsigned int i=0; i < target.size(); i++){
+    for(unsigned int i=0; i < target.size(); i++) {
         route.push_back(target[i]);
     }
 
@@ -258,16 +240,14 @@ vector<char> iServer(){
     return route;  //returned een vector<char> met de route (wss naar de Navigation() functie)
 }
 
-
 //---------------------------------------DRIVING---------------------------------------------
 
-void fwd(const int lspd, const int rspd){
+void fwd(const int lspd, const int rspd) {
     BP.set_motor_power(PORT_B, lspd);
     BP.set_motor_power(PORT_C, rspd);
 }
 
-
-void turnLeft(){
+void turnLeft() {
     fwd(-20, -20);
     sleep(1);
     resetMotor();
@@ -275,7 +255,8 @@ void turnLeft(){
     fwd(20, -60);
     sleep(7);
 }
-void turnRight(){
+
+void turnRight() {
     fwd(-20, -20);
     sleep(1);
     resetMotor();
@@ -283,7 +264,6 @@ void turnRight(){
     fwd(-60, 20);
     sleep(7);
 }
-
 
 int moveForward() {
   //Aan de hand van pid controller
@@ -308,7 +288,7 @@ int moveForward() {
   while (running) {
     if (BP.get_sensor(PORT_3, Light3) == 0) {
       lightvalue = Light3.reflected; // neem waarde van zwartwit sensor
-      if (BP.get_sensor(PORT_1, Color1) == 0){
+      if (BP.get_sensor(PORT_1, Color1) == 0) {
         if ((Color1.color == 1 || Color1.color == 2) && (lightvalue > 2300)) { // als de zwartwit sensor en de kleur sensor zwart zijn is er een kruispunt
           cout << "hier is een kruispunt" << endl;
           return 0;
@@ -336,68 +316,63 @@ int moveForward() {
   }
 }
 
-
-void Drive(char direction){
-    if (direction == 'f'){
+void drive(char direction) {
+    if (direction == 'f') {
         //ga 1 grid plek
         moveForward();
     }
-    else if (direction == 'r'){
+    else if (direction == 'r') {
         //ga 90 graden links
         turnRight();
     }
-    else if (direction == 'l'){
+    else if (direction == 'l') {
         //ga 90 graden links
         turnLeft();
     }
-    else if (direction == 'b'){
+    else if (direction == 'b') {
         //ga 180 graden draaien
         turnRight();
         turnRight();
-    }
-    else{
+    } else {
         cout << "Invalid operator: " << direction << endl << "Next time use: f, r, l or b" << endl;
     }
 }
 
-
-void Navigation(vector<char> route){
+void navigation(vector<char> route) {
     route.insert(route.begin(), 1, 'n');    //zorg dat ook het eerste echte coordinaat een relatief punt heeft om vanaf te bewegen
     for (int i = 1; i < route.size(); ++i) { // rij naar het object toe aan de hand van de route
-        if (route[i] == route[i-1]){
-            Drive('f');
-        }
-        else {
+        if (route[i] == route[i-1]) {
+            drive('f');
+        } else {
             resetMotor();
             if
                     ((route[i] == 'n' && route[i-1] == 'w') ||
                      (route[i] == 'o' && route[i-1] == 'n') ||
                      (route[i] == 's' && route[i-1] == 'o') ||
-                     (route[i] == 'w' && route[i-1] == 's')){
-                Drive('r');
-                Drive('f');
+                     (route[i] == 'w' && route[i-1] == 's')) {
+                drive('r');
+                drive('f');
             }
             else if
                     ((route[i] == 'n' && route[i-1] == 'o') ||
                      (route[i] == 'o' && route[i-1] == 's') ||
                      (route[i] == 's' && route[i-1] == 'w') ||
-                     (route[i] == 'w' && route[i-1] == 'n')){
-                Drive('l');
-                Drive('f');
+                     (route[i] == 'w' && route[i-1] == 'n')) {
+                drive('l');
+                drive('f');
             }
             else if
                     ((route[i] == 'n' && route[i-1] == 's') ||
                      (route[i] == 'o' && route[i-1] == 'w') ||
                      (route[i] == 's' && route[i-1] == 'n') ||
-                     (route[i] == 'w' && route[i-1] == 'o')){
-                Drive('b');
-                Drive('f');
-            } else{ // als geen van de opties gepakt word gebeurt er iets abnormaals
+                     (route[i] == 'w' && route[i-1] == 'o')) {
+                drive('b');
+                drive('f');
+            } else { // als geen van de opties gepakt word gebeurt er iets abnormaals
                 cout << "help, er gebeurt iets geks: " << route[i] << route[i-1] << endl;
             }
         }
     }
-
 
     cout << "Arrived at destination" << endl;
     sleep(1);
@@ -408,7 +383,7 @@ void Navigation(vector<char> route){
     //***************************************************************************************************************
 
 
-    
+
     //***************************************************************************************************************
                             // functie voor object zien en vooral oppakken
     brengNaarKantelPunt();
@@ -422,13 +397,11 @@ void Navigation(vector<char> route){
     //***************************************************************************************************************
     cout << "Picked up ze object, time to head back" << endl;
 
-
     route.push_back('n'); //zorg dat ook het eerste echte coordinaat een relatief punt heeft om vanaf te bewegen
     for (int i = (route.size() - 2); i > 0; --i) { // rij terug naar het startpunt aan de hand van de route
-        if (route[i] == route[i+1]){
-            Drive('f');
-        }
-        else {
+        if (route[i] == route[i+1]) {
+            drive('f');
+        } else {
             resetMotor();
             sleep(1);
             if
@@ -436,25 +409,25 @@ void Navigation(vector<char> route){
                      (route[i] == 'o' && route[i+1] == 'n') ||
                      (route[i] == 's' && route[i+1] == 'o') ||
                      (route[i] == 'w' && route[i+1] == 's')){
-                Drive('l');
-                Drive('f');
+                drive('l');
+                drive('f');
             }
             else if
                     ((route[i] == 'n' && route[i+1] == 'o') ||
                      (route[i] == 'o' && route[i+1] == 's') ||
                      (route[i] == 's' && route[i+1] == 'w') ||
                      (route[i] == 'w' && route[i+1] == 'n')){
-                Drive('r');
-                Drive('f');
+                drive('r');
+                drive('f');
             }
             else if
                     ((route[i] == 'n' && route[i+1] == 's') ||
                      (route[i] == 'o' && route[i+1] == 'w') ||
                      (route[i] == 's' && route[i+1] == 'n') ||
                      (route[i] == 'w' && route[i+1] == 'o') ){
-                Drive('b');
-                Drive('f');
-            } else{  // als geen van de opties gepakt word gebeurt er iets abnormaals
+                drive('b');
+                drive('f');
+            } else {  // als geen van de opties gepakt word gebeurt er iets abnormaals
                 cout << "help, er gebeurt iets geks: " << route[i] << route[i+1] << endl;
             }
         }
@@ -474,84 +447,78 @@ void Navigation(vector<char> route){
 
     //***************************************************************************************************************
 
-    Drive('b'); // orienteer jezelf goed voor de volgende missie
+    drive('b'); // orienteer jezelf goed voor de volgende missie
     resetMotor();
 
 }
-
 
 //---------------------------------------MAIN---------------------------------------------
 
 int main() {
     signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
-
     BP.detect();
     BP.reset_all();
+    for (int i = 0; i < 5; ++i) {
+      cout << ".";
+      if (i == 3) {
+        setSensors();
+      }
+      sleep(1);
+    }
+    cout << endl << "Initialized" << endl;
 
     BP.set_motor_limits(PORT_B, 50, 0);
     BP.set_motor_limits(PORT_C, 50, 0);
 
-    setSensors();
     resetMotor();
     thread checkBattery(batteryLevel);
 
-    // mooie manier om even te wachten tot alle sensor zijn ge initialiseerd en goed werken.
-    cout << endl << "Initializing" << endl;
-    int o = 10;
-    while (o != 0){
-        --o;
-        cout << '.';
-        sleep(0.5);
-    }
-    cout << endl << "Initialized" << endl << endl;
-
     int uChoice;
 
-    while (running){
+    while (::running) {
         cout << endl << "Kies een van deze functies: " << endl;
-        cout << "1: Receive message" << endl; // wacht voor een message en print deze
-        cout << "2: Set communication details" << endl;  // stel portnummer in
-        cout << "3: Wait for message (route) , then return the object" << endl; // Er wordt eerst gewacht op een bericht met de coordinaten, die worden daarna gebruikt om het opject op te pakken en terug te rijden
-        cout << "4: Drive the given route" << endl;  // dit is een case die gebruikt wordt voor testing en demo's maar kan weg weg in eindproduct
+        cout << "0: Exit" << endl;                                              // functie om programma te stoppen
+        cout << "1: Receive message" << endl;                                   // wacht voor een message en print deze
+        cout << "2: Set communication details" << endl;                         // stel portnummer in
+        cout << "3: Wait for message (route) , then return the object" << endl; // Er wordt eerst gewacht op een bericht met de coordinaten,
+                                                                                // die worden daarna gebruikt om het opject op te pakken en terug te rijden
+        cout << "4: Drive the given route" << endl;                             // dit is een case die gebruikt wordt voor testing en demo's maar kan weg weg in eindproduct
         cout << "5: Check sensor" << endl;
-        cout << "0: Stop it and die" << endl; // functie om programma te stoppen
         cout << endl << "Uw keuze is: ";
 
         cin >> uChoice; cout << endl << endl;
 
         switch(uChoice) {
-            case 1:   // wacht voor een message en print deze
+            case 1: // wacht voor een message en print deze
                 iServer();
                 break;
-            case 2:   // stel portnummer in
+            case 2: // stel portnummer in
                 SetComm();
                 break;
-            case 3:    // Er wordt eerst gewacht op een bericht met de coordinaten, die worden daarna gebruikt om het opject op te pakken en terug te rijden
-                Navigation(iServer());
+            case 3: // Er wordt eerst gewacht op een bericht met de coordinaten,
+                    //die worden daarna gebruikt om het opject op te pakken en terug te rijden
+                navigation(iServer());
                 break;
-            case 4: { // dit is een case die gebruikt wordt voor testing en demo's maar dit kan weg in eindproduct
+            case 4:  // dit is een case die gebruikt wordt voor testing en demo's maar dit kan weg in eindproduct
                 vector<char> vec = {'n', 'n', 'w', 'w', 's'};
-                Navigation(vec);
+                navigation(vec);
                 break;
-            }
-            case 5:    // Check the sensors
+            case 5: // Check the sensors
                 checkSensor();
                 break;
-            case 0: {  // functie om programma te stoppen
+            case 0: // functie om programma te stoppen
                 ::running = false;
                 break;
-            }
         }
     }
 }
 
-
 // Signal handler that will be called when Ctrl+C is pressed to stop the program
-void exit_signal_handler(int signo){
+void exit_signal_handler(int signo) {
     if(signo == SIGINT){
         resetMotor();
         ::running = false;
-        BP.reset_all();    // Reset everything so there are no run-away motors
+        BP.reset_all(); // Reset everything so there are no run-away motors
         exit(-2);
     }
 }
