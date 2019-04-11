dc6f3f5f579850f233cd70c8c859cd2e537a8c48
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2019-04-11 19:05:04 +0200

    changed where resetMotors() is called.

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -786,6 +786,7 @@ void followRoute(string & followedRoute, bool & destinationArrived, gridPoints &
 			else{
 				move(directions[i], GP);
 				followedRoute += directions[i];
+				resetMotors();
 
 				if(i == directions.size() - 1){
 					destinationArrived = true;
@@ -797,7 +798,6 @@ void followRoute(string & followedRoute, bool & destinationArrived, gridPoints &
 		}
 		
 	}
-	resetMotors();
 }
 
 void dockScout(gridPoints & GP){
