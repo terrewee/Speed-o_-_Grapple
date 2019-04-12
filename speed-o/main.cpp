b0e4ab8837c83b5bc5e6213312f9515e22f20b2d
Terrewee <terrewee@live.com>
2019-04-12 10:23:57 +0200

    Update main.cpp

--- speed-o/main.cpp
+++ speed-o/main.cpp
@@ -310,8 +310,8 @@ void turnLeft(gridPoints & GP){
 
 	BP.get_motor_encoder(PORT_B);
 	BP.get_motor_encoder(PORT_C);
-	BP.set_motor_position_relative(PORT_B, -110);
-	BP.set_motor_position_relative(PORT_C, 110);
+	BP.set_motor_position_relative(PORT_B, -130);
+	BP.set_motor_position_relative(PORT_C, 130);
 	sleep(1);
 }
 
@@ -332,8 +332,8 @@ void turnRight(gridPoints & GP){
 
 	BP.get_motor_encoder(PORT_B);
 	BP.get_motor_encoder(PORT_C);
-	BP.set_motor_position_relative(PORT_B, 110);
-	BP.set_motor_position_relative(PORT_C, -110);
+	BP.set_motor_position_relative(PORT_B, 130);
+	BP.set_motor_position_relative(PORT_C, -130);
 	sleep(1);
 }
 
@@ -533,8 +533,8 @@ string manualControl(gridPoints &GP){
 	while(true){
 		cin >> answer;
 		if 			(answer == "w")		{followLine(1);}
-		else if (answer == "a")		{moveForward(10,10); sleep(0.2); resetMotors(); turnLeft(GP); followLine(1);}
-		else if (answer == "d")		{moveForward(10,10); sleep(0.2); resetMotors(); turnRight(GP); followLine(1);}
+		else if (answer == "a")		{turnLeft(GP); followLine(1);}
+		else if (answer == "d")		{turnRight(GP); followLine(1);}
 		else if (answer == "esc")	{break;}
 		else 											{cout << "invalid input." << endl; continue;}
 
@@ -1017,7 +1017,7 @@ int main(){
 
     switch(uChoice) {
 			case 0:
-        runProgram = false;
+        {runProgram = false;}
 				break;
       case 1:
         cout << "Message: " << endl;
